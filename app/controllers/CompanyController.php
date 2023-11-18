f96bc79a2eba977e27c7f73662bed8a018b04488
rifat <beastthe588@gmail.com>
2023-11-19 01:31:47 +0200

    adding booking and the final update

--- app/controllers/CompanyController.php
+++ app/controllers/CompanyController.php
@@ -12,6 +12,7 @@ class CompanyController{
     }
     public function addCompany(){
         if($_SERVER["REQUEST_METHOD"] == "POST"){
+            $this->testPost(["title","address","phone"]);
            $data = ["title"=>$_POST["title"],"address"=>$_POST["address"],"phone"=>$_POST["phone"]];
            echo $this->toJson($this->company_model->addCompany($data));
         }
@@ -19,18 +20,12 @@ class CompanyController{
          echo "No Data To Be Add";
     }
     public function deleteCompany(){
-        if($_SERVER["REQUEST_METHOD"] == "POST")
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $this->testPost(["id"]);
         echo $this->toJson($this->company_model->deleteCompany($_POST["id"]));
+    }
         else 
          echo "No Data To Be Delete";
-    }
-    public function updateCompany(){
-        if($_SERVER["REQUEST_METHOD"] == "POST"){
-            $data = ["title"=>$_POST["title"], "address"=>$_POST["address"],"phone"=>$_POST["phone"]];
-         echo $this->toJson($this->company_model->updateCompany($_POST["id"],$data));
-        }
-        else 
-         echo "No Data To Be Update";
     }
     public function getCompany(){
         $data = $this->company_model->getCompany();
--- app/controllers/CustomerController.php
+++ app/controllers/CustomerController.php
@@ -12,6 +12,7 @@ class CustomerController{
     }
     public function addCustomer(){
         if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $this->testPost(["name","mobile","gender","email"]);
        $data = ["name"=>$_POST["name"],"mobile"=>$_POST["mobile"],"gender"=>$_POST["gender"],"email"=>$_POST["email"]];
        echo $this->toJson($this->customer_model->addCustomer($data));
         }
@@ -19,13 +20,16 @@ class CustomerController{
          echo "No Data To Be Add";
     }
     public function deleteCustomer(){
-        if($_SERVER["REQUEST_METHOD"] == "POST")
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $this->testPost(["id"]);
         echo $this->toJson($this->customer_model->deleteCustomer($_POST["id"]));
+        }
         else 
          echo "No Data To Be Delete";
     }
     public function updateCustomer(){
         if($_SERVER["REQUEST_METHOD"] == "POST"){
+            $this->testPost(["name","mobile","gender","email"]);
             $data = ["name"=>$_POST["name"],"mobile"=>$_POST["mobile"],"gender"=>$_POST["gender"],"email"=>$_POST["email"]];
             echo $this->toJson($this->customer_model->updateCustomer($_POST["id"],$data));
         }
